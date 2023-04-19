bdb2adf6650259de5ba9dbb9545d864981c4f91b
TimWang95 <tgm761@gmail.com>
2023-04-19 11:13:19 +0800

    feat: add change mode handler

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -81,6 +81,20 @@ const TodoPage = () => {
       })
     })
   }
+  const handleChangeMode = ({ id, isEdit }) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            isEdit,
+          }
+        }
+
+        return {...todo, isEdit: false}
+      })
+    })
+  }
   return (
     <div>
       TodoPage
@@ -91,7 +105,11 @@ const TodoPage = () => {
         onAddTodo={handleAddTodo} 
         onKeyDown={handleKeyDown}
       />
-      <TodoCollection todos={todos} onToggleDone={handleToggleDone} />
+      <TodoCollection 
+        todos={todos} 
+        onToggleDone={handleToggleDone} 
+        onChangeMode={handleChangeMode}  
+      />
       <Footer /> 
     </div>
   );
