6f8b0116de62ab5ee8cad70afa9cf8c8b1ea6e12
TimWang95 <tgm761@gmail.com>
2023-04-19 11:22:18 +0800

    feat: add save handler

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -95,6 +95,22 @@ const TodoPage = () => {
       })
     })
   }
+  const handleSave = ({ id, title }) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            id,
+            title,
+            isEdit: false,
+          }
+        }
+
+        return todo
+      })
+    })
+  }
   return (
     <div>
       TodoPage
@@ -107,6 +123,7 @@ const TodoPage = () => {
       />
       <TodoCollection 
         todos={todos} 
+        onSave={handleSave}
         onToggleDone={handleToggleDone} 
         onChangeMode={handleChangeMode}  
       />
