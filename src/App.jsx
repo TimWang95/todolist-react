62f1812c0df5e4a050f0604eecc70f6c9865d5b4
TimWang95 <tgm761@gmail.com>
2023-05-23 19:19:29 +0800

    fix path of Todopage

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <div className="app">
       <BrowserRouter>
         <Routes>
-          <Route path="todo" element={<TodoPage />} />
+          <Route path="todos" element={<TodoPage />} />
           <Route path="login" element={<LoginPage />} />
           <Route path="signup" element={<SignUpPage />} />
           <Route path="*" element={<HomePage />} />
